4ba6067d8ec053ea0fcfd456fecce8659e9102e3
Nico Ring <ringnico@web.de>
2015-06-10 13:34:40 +0200

    lern to merge

--- public/js/core/main.js
+++ public/js/core/main.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import Router from 'react-router';
-<<<<<<< HEAD
 import routes from './routes';
-import ListActions from '../actions/listActions';
-
-const listsUrl = '/api/lists';
-
-fetch(listsUrl)
-  .then(response => response.json())
-  .then(json => {
-    console.log(json);
-    ListActions.updateLists(json)
-  });
-=======
-import routes from './Routes';
->>>>>>> 4020448a04f374cb81804a665208373d4321dbf0
 
 Router.run(routes, (Handler) => React.render(<Handler /> , document.body));
